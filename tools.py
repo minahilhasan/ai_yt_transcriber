6d4b1f732381283d25498824883b69a25362ec0d
minahilhasan <minahilhasan@gmail.com>
2025-12-23 22:54:22 +0500

    Update

--- tools.py
+++ tools.py
@@ -26,7 +26,7 @@ def videotranscriber(youtube_url):
     client = genai.Client(api_key=st.secrets["GEMINI"]["GEMINI_API_KEY"])
 
     response = client.models.generate_content(
-        model='gemini-gemini-2.0-flash-lite', 
+        model='gemini-1.5-flash', 
         contents=[
             types.Part.from_uri(
                 file_uri=youtube_url,
